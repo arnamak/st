4f4ff0daa446bd416ac1e048620e90f52f0781c1
Mak Arnautovic <me@mak.io>
2021-04-17 19:52:11 -0500

    Add copyurl

--- config.def.h
+++ config.def.h
@@ -193,6 +193,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
+	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
 };
 
 /*

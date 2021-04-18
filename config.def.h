1210b44d464c176bb798c7ec458cd504de07129a
Mak Arnautovic <me@mak.io>
2021-04-17 20:05:04 -0500

    Add open_copied_url

--- config.def.h
+++ config.def.h
@@ -196,6 +196,7 @@ static Shortcut shortcuts[] = {
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 	{ TERMMOD,              XK_Return,      newterm,        {.i =  0} },
 	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
+	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 };
 
 /*
